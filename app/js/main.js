47a1f66a7d1c1bed06b828884918e346245894f4
PavelMaryin <p.maryin@gmail.com>
2021-07-20 00:59:11 +0300

    fixed mistakes

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ $(function(){
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
-    fade: true,
-    autoplay: true
+    // fade: true,
+    // autoplay: true
   })
 
   var mixer = mixitup('.week-products__list');
